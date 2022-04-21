102a66f7ee1292722afe0584099965c007d1645c
ZekeZeke <zvandehy@gmail.com>
2022-04-21 08:47:47 -0400

    fix backend link

--- react/nba-analyzer/src/index.js
+++ react/nba-analyzer/src/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
 // 1
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
-  InMemoryCache
-} from '@apollo/client';
+  InMemoryCache,
+} from "@apollo/client";
 
 // 2
 const httpLink = createHttpLink({
-  // uri: 'https://datatrain-nba-yxh2z.ondigitalocean.app/query'
-  uri: "http://localhost:8080/query"
+  uri: "https://datatrain-nba-yxh2z.ondigitalocean.app/query",
+  // uri: "http://localhost:8080/query"
 });
 
 // 3
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 // 4
 ReactDOM.render(
   <BrowserRouter>
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-</BrowserRouter>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
