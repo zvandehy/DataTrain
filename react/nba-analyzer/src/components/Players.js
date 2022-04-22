2df6fc1a25dd9b92999bda411152cd2541e1d1df
ZekeZeke <zvandehy@gmail.com>
2022-04-22 17:08:58 -0400

    add sorting

--- react/nba-analyzer/src/components/Players.js
+++ react/nba-analyzer/src/components/Players.js
@@ -3,15 +3,63 @@ import Playercard from "./Playercard";
 import DataListInput from "react-datalist-input";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSortUp as asc,
+  faSortDown as desc,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   HOME_QUERY,
   FormatDate,
   ParseDate,
   GetSelectableTeams,
+  AveragePropScore,
 } from "../utils.js";
 import { StatObjects } from "../predictions.js";
 import { useQuery } from "@apollo/client";
 import "../styles/players.css";
+
+function GetSortMethod(method) {
+  switch (method) {
+    case "player":
+      return (a, b) => {
+        if (a.player.name < b.player.name) return -1;
+        if (a.player.name > b.player.name) return 1;
+        return 0;
+      };
+    case "position":
+      return (a, b) => {
+        if (a.player.position < b.player.position) return -1;
+        if (a.player.position > b.player.position) return 1;
+        return 0;
+      };
+    case "team":
+      return (a, b) => {
+        if (
+          a.player.currentTeam.abbreviation < b.player.currentTeam.abbreviation
+        )
+          return -1;
+        if (
+          a.player.currentTeam.abbreviation > b.player.currentTeam.abbreviation
+        )
+          return 1;
+        return 0;
+      };
+    case "opponent":
+      return (a, b) => {
+        if (a.opponent.abbreviation < b.opponent.abbreviation) return -1;
+        if (a.opponent.abbreviation > b.opponent.abbreviation) return 1;
+        return 0;
+      };
+    default:
+      return (a, b) => {
+        return (
+          AveragePropScore(a.player.games, method) -
+          AveragePropScore(b.player.games, method)
+        );
+      };
+  }
+}
 
 const Players = () => {
   const [teamLookup, setTeamLookup] = useState("ANY");
@@ -19,6 +67,8 @@ const Players = () => {
   const [statPreference, setStatPreference] = useState("");
   const [positionLookup, setPositionLookup] = useState("");
   const [searchLookup, setSearchLookup] = useState("");
+  const [sortMethod, setSortMethod] = useState("team");
+  const [sortOrder, setSortOrder] = useState("DESC");
   const [date, setDate] = useState(FormatDate(new Date()));
   const { loading, error, data, refetch } = useQuery(HOME_QUERY, {
     variables: { date: date },
@@ -76,9 +126,23 @@ const Players = () => {
               );
             })
           : filterByPosition;
-      setShowPlayers(filterBySearch);
+
+      let sorted = filterBySearch.sort((a, b) => {
+        return GetSortMethod(sortMethod)(a, b);
+      });
+
+      sorted = sortOrder === "DESC" ? sorted.reverse() : sorted;
+      setShowPlayers(sorted);
     }
-  }, [data, teamLookup, statPreference, positionLookup, searchLookup]);
+  }, [
+    data,
+    teamLookup,
+    statPreference,
+    positionLookup,
+    searchLookup,
+    sortMethod,
+    sortOrder,
+  ]);
 
   function changeDate(date) {
     date = FormatDate(date);
@@ -119,6 +183,10 @@ const Players = () => {
     }
   }
 
+  function onSelectSort(input) {
+    setSortMethod(input.value);
+  }
+
   if (loading) return "Loading...";
   if (error) {
     return `Error! ${error.message}. ${loading}. ${data}`;
@@ -132,6 +200,18 @@ const Players = () => {
     value: item,
   }));
   selectableStats.unshift({ key: "ANY", label: "ANY", value: "" });
+
+  let selectSorts = [
+    { key: "team", label: "Team", value: "team" },
+    { key: "opponent", label: "Opponent", value: "opponent" },
+    { key: "player", label: "Player Name", value: "player" },
+    { key: "position", label: "Position", value: "position" },
+  ];
+  selectSorts = selectSorts.concat(
+    StatObjects.map((item) => {
+      return { key: item.recognize, label: item.label, value: item.recognize };
+    })
+  );
   return (
     <div className="players">
       <div className="teams-dropdown">
@@ -180,6 +260,26 @@ const Players = () => {
           placeholder={"Filter by keyword"}
           onChange={(e) => setSearchLookup(e.target.value)}
         />
+        <div className="sorter">
+          <DataListInput
+            placeholder="Sort players by"
+            items={selectSorts}
+            onSelect={onSelectSort}
+            clearInputOnClick={true}
+            suppressReselect={false}
+            value={sortMethod.toUpperCase()}
+          />
+          <button
+            onClick={() => setSortOrder(sortOrder === "DESC" ? "ASC" : "DESC")}
+          >
+            {sortOrder === "DESC" ? (
+              <FontAwesomeIcon icon={asc} />
+            ) : (
+              <FontAwesomeIcon icon={desc} />
+            )}
+          </button>
+        </div>
+        {/* TODO: Add Sort by projection value (instead of season average) & sort by prediction (overUnder / confidence) */}
       </div>
       <ul className="players-list">
         {showPlayers.length > 0 ? (
