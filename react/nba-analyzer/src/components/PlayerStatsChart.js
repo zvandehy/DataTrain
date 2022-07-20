da0e8b83bba9dc0b1d0ca6bc46a5f2d9cb402335
ZekeZeke <zvandehy@gmail.com>
2022-07-19 18:53:00 -0600

    Player Stats Chart Styling (#85)

--- react/nba-analyzer/src/components/PlayerStatsChart.js
+++ react/nba-analyzer/src/components/PlayerStatsChart.js
@@ -56,91 +56,123 @@ const PlayerStatsChart = (props) => {
         },
       },
       title: {
-        display: true,
-        text: "Player Stats Chart",
+        display: false,
+        // text: `${}`,
+        // color: "#FFF",
       },
       tooltip: {
         enabled: true,
         mode: "index",
-        // callbacks: {
-        //     title: () => "Title",
-        //     label: (context) => {},
-        //     }
-      },
-    },
-    scales: {
-      y: {
-        minutes: { display: false, min: 0, max: 48, type: "linear" },
-        percent: { display: false, min: 0, max: 1, type: "linear" },
-        display: false,
-        min: 0,
-        type: "linear",
-      },
-    },
-  };
-  const dates = games.map((game) => game.date);
-  const lines = ["minutes", "field_goal_percentage"];
-  let datasets = Object.getOwnPropertyNames(games[0])
-    .filter((item) => item !== "__typename" && item !== "date")
-    .map((prop, i) => {
-      const r = Math.floor(Math.random() * 255);
-      const g = Math.floor(Math.random() * 255);
-      const b = Math.floor(Math.random() * 255);
-      return {
-        id: prop,
-        label: prop,
-        type: lines.find((item) => item === prop) ? "line" : "line",
-        fill: true,
-        data: games.map((game) => GetPropScore(game, prop)),
-        yAxisID:
-          prop.indexOf("percent") > -1 || prop === "usage"
-            ? "percent"
-            : prop === "minutes"
-            ? prop
-            : "y",
-        hidden: prop !== selected.recorgnize,
-        order: prop !== selected.recognize ? (prop === "minutes" ? 100 : i) : 0,
-        backgroundColor: `rgba(${r}, ${g}, ${b}, 0.5)`,
-        color: `rgba(${r}, ${g}, ${b}, 0.5)`,
-        fillColor: `rgba(${r}, ${g}, ${b}, 0.5)`,
-        borderWidth: 2,
-        pointBorderColor: function (context) {
-          const index = context.dataIndex;
-          const value = context.dataset.data[index];
-          const prediction = predictions.find(
-            (prediction) => prediction.stat.recognize === prop
-          );
-          if (prediction === undefined) {
-            return `rgba(${r}, ${g}, ${b}, 0.5)`;
-          }
-          return value <= prediction.target
-            ? "rgba(255,0,0,0.5)"
-            : "rgba(0,255,0,0.5)";
+        callbacks: {
+          // label: function (context) {
+          //   return "Hello world";
+          // },
+          title: function (context) {
+            const index = context[0].dataIndex;
+            console.log(context, index, games.length, games[index]);
+            const game = games[index];
+            return `${game.team.abbreviation} ${game.margin > 0 ? "+" : ""}${
+              game.margin
+            } ${game.home_or_away === "home" ? "vs" : "@"} ${
+              game.opponent.abbreviation
+            } ${game.date}`;
+          },
         },
-      };
-    });
-
-  // let propDataset = datasets[datasets.findIndex(item => item.id === prop)]
-  // for (let i=0; i<datasets[datasets.findIndex(item => item.id === prop)].data.length; i++) {
-  //     if (GetPropScore({points:datasets[datasets.findIndex(item => item.id === prop)].data[i]},prop) > target) {
-  //         datasets[datasets.findIndex(item => item.id === prop)].pointBackgroundColors.push("rgba(0,255,0,0.5)")
-  //     } else {
-  //         datasets[datasets.findIndex(item => item.id === prop)].pointBackgroundColors.push("rgba(255,0,0,0.5)")
-  //     }
-  // datasets[datasets.findIndex(item => item.id === prop)].pointBackgroundColor = GetPropScore({points:datasets[datasets.findIndex(item => item.id === prop)].data[i]},prop) > 20 ? 'green' : 'red'
-  // }
+      },
+    },
+    // scales: {
+    //   y: {
+    //     minutes: { display: false, min: 0, max: 48, type: "linear" },
+    //     percent: { display: false, min: 0, max: 1, type: "linear" },
+    //     display: false,
+    //     min: 0,
+    //     type: "linear",
+    //   },
+    // },
+  };
+  const colors = [
+    "#5D2E8C",
+    "#1B998B",
+    "#3185FC",
+    "#E94900",
+    "#16DB93",
+    "#83E377",
+    "#FF7B9C",
+    "#EFEA5A",
+    "#F29E4C",
+    "#F29E4C",
+  ];
+  const labels = games.map((game) => `${game.opponent.abbreviation}`);
+  let datasets = [
+    "Fantasy Score",
+    "Points",
+    "Rebounds",
+    "Assists",
+    "Steals",
+    "Blocks",
+    "Turnovers",
+    "Minutes",
+    "Free Throws Made",
+    "3-PT Made",
+    "Three_Pointers_Attempted",
+  ].map((prop, i) => {
+    return {
+      id: prop,
+      label: prop,
+      type:
+        prop.toLowerCase() === selected.recognize?.toLowerCase()
+          ? "bar"
+          : "line",
+      data: games.map((game) => GetPropScore(game, prop)),
+      yAxisID:
+        prop.indexOf("percent") > -1 || prop === "usage"
+          ? "percent"
+          : prop === "minutes"
+          ? prop
+          : "y",
+      hidden: prop !== selected.recorgnize,
+      order: prop.toLowerCase() !== selected.recognize.toLowerCase() ? 0 : 1,
+      backgroundColor: `${colors[i % colors.length]}`,
+      borderColor: function (context) {
+        if (prop.toLowerCase() !== selected.recognize?.toLowerCase()) {
+          return `${colors[i % colors.length]}`;
+        }
+        return graphColor(context, predictions, prop, colors, i);
+      },
+      borderWidth: 2,
+      pointBorderColor: function (context) {
+        return graphColor(context, predictions, prop, colors, i);
+      },
+    };
+  });
   return (
-    <div id="chart">
-      <Chart
-        datasetIdKey="id"
-        options={options}
-        data={{
-          labels: dates.map((item, i) => i),
-          datasets: datasets,
-        }}
-      />
+    <div id="chart-wrapper">
+      <div id="chart">
+        <Chart
+          datasetIdKey="id"
+          options={options}
+          data={{
+            labels: labels,
+            datasets: datasets,
+          }}
+          color={"#FFFFFF"}
+        />
+      </div>
     </div>
   );
 };
+
+function graphColor(context, predictions, prop, colors, i) {
+  const index = context.dataIndex;
+  const value = context.dataset.data[index];
+  const prediction = predictions.find(
+    (prediction) =>
+      prediction.stat.recognize.toLowerCase() === prop.toLowerCase()
+  );
+  if (prediction === undefined) {
+    return `${colors[i % colors.length]}`;
+  }
+  return value <= prediction.target ? "rgba(255,0,0)" : "rgba(0,255,0, .8)";
+}
 
 export default PlayerStatsChart;
