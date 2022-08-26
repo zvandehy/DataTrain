430ab9f71a743117eef2c23f9e5ecc0633188a9f
ZekeZeke <zvandehy@gmail.com>
2022-08-26 14:45:50 -0500

    fly ui

--- clover-analytics/src/shared/apollo-client.ts
+++ clover-analytics/src/shared/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://clover-backend.fly.dev/wnba/query",
+  uri: "https://clover-backend.fly.dev/wnba/query",
   // uri: "http://localhost:8080/wnba/query",
   cache: new InMemoryCache(),
 });
