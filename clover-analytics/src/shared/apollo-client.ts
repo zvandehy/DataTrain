6e96b6ba18d2f29ffc1e122a0d3228c79e5ae213
ZekeZeke <zvandehy@gmail.com>
2022-08-28 10:13:43 -0500

    use live backend

--- clover-analytics/src/shared/apollo-client.ts
+++ clover-analytics/src/shared/apollo-client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  // uri: "https://clover-backend.fly.dev/wnba/query",
-  uri: "http://localhost:8080/wnba/query",
+  uri: "https://clover-backend.fly.dev/wnba/query",
+  // uri: "http://localhost:8080/wnba/query",
   cache: new InMemoryCache(),
 });
 
