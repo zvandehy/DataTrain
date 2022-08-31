0a718b077bf6dc39011daf0a41d4cd27dba288e0
ZekeZeke <zvandehy@gmail.com>
2022-08-31 13:38:40 -0400

    try redirects

--- clover-analytics/src/shared/apollo-client.ts
+++ clover-analytics/src/shared/apollo-client.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://clover-backend.fly.dev/wnba/query",
+  // uri: "https://clover-backend.fly.dev/wnba/query",
   // uri: "http://localhost:8080/wnba/query",
+  uri: "/api/wnba",
   cache: new InMemoryCache(),
 });
 
